1f42bb9796ac6ce9e9052a3220dc3b88bb023449
Achiya Hevroni <110335625+achiyaBSII@users.noreply.github.com>
2025-04-28 20:49:27 +0000

    SFN's oases-config suggestion

---
+++ oases-config SFN suggestions/DeviceModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConfigurationSuggestion
+{
+    public class DeviceModel
+    {
+        public EntityCategory Category { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime? YearOfManufacture { get; set; }
+        public string? Description { get; set; }
+        public string? Comment { get; set; }
+        List<DataPoint> DataPoints { get; set; }
+    }
+}
