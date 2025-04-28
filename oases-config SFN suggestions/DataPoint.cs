9fd96ebd0c5a1ca5382da719ab796ff3fb0a8a80
Achiya Hevroni <110335625+achiyaBSII@users.noreply.github.com>
2025-04-28 20:49:27 +0000

    SFN's oases-config suggestion

---
+++ oases-config SFN suggestions/DataPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace ConfigurationSuggestion
+{
+    public enum DataType
+    {
+        AnalogMeasurement,
+        DigitalMeasurement
+    }
+
+    public class ValueBoundaries
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+    }
+
+    public class DataPoint
+    {
+        public uint? Index { get; set; }
+        public string? Name { get; set; }
+        public DataType Type { get; set; }
+        public QuantityName? Unit { get; set; }
+        public ValueBoundaries? Limitations { get; set; }
+    }
+}
